3ce777575a41063d563a6420546abd321b749476
Conner Brooks <connerbrooks@gmail.com>
2016-03-15 16:45:43 -0400

    Initialize intrinsics and extrinsics.

--- include/settings.hpp
+++ include/settings.hpp
@@ -9,7 +9,8 @@ namespace ISUE {
       Settings() : num_trees_(5), max_tree_depth_(16), 
         num_frames_per_tree_(500), num_pixels_per_frame_(5000),
         image_width_(640), image_height_(480),
-        depth_factor_(5000)
+        depth_factor_(5000),
+        fx(525.0f), fy(525.0f), cx(319.5f), cy(239.5f)
       {}
 
       int32_t num_trees_,
